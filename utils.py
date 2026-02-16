e2cc61b6302d01bcba227cc2329700d4da58229a
Dawid Maleszka <dawidm91@gmail.com>
2026-02-16 13:05:46 +0100

    elemrnty szkolenia

--- utils.py
+++ utils.py
@@ -37,7 +37,7 @@ class GPTDatasetV1(Dataset):
 
 
 def create_dataloader_v1(txt, batch_size=4, max_lenght=256, stride=128, shuffle=True, drop_last=True, num_workers=0):
-    tokenizer = tiktoken.get_encoding("cl100k_base")
+    tokenizer = tiktoken.get_encoding("gpt2")
     dataset = GPTDatasetV1(txt, tokenizer, max_lenght, stride)
     dataloader = DataLoader(
         dataset,
